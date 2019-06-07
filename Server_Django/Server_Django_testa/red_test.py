ea4c2e89fea54ecd24bdc297e9675832d84fcf20
Deepbert <Deepbert@gmail.com>
2019-06-07 14:23:33 +0300

    Add pytest. Delete unittest. Connect bootstrap.

--- Server_Django/Server_Django_testa/red_test.py
+++ Server_Django/Server_Django_testa/red_test.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 
 @pytest.fixture
@@ -11,7 +12,16 @@ def test_title(browser):
 
     browser.get('http://127.0.0.1:8000/home/')
 
+    header_text = browser.find_element_by_tag_name('h1').text
+
+    inputbox = browser.find_element_by_id('First_ID')
+
+    inputbox.send_keys('HHHhhh')
+
+    assert header_text == 'Server_hh'
+
     assert browser.title == 'Server4'
+
 
 
 
